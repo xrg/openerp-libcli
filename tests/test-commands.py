63ff3b9b6063d23a502319489ca073ad90676c5c
P. Christeas <xrg@linux.gr>
2011-07-28 01:16:22 +0300

    tests: port "test-commands" to module now in API

--- tests/test-commands.py
+++ tests/test-commands.py
@@ -7,7 +7,7 @@ import logging
 import time
 logging.basicConfig(level=logging.DEBUG)
 
-from openerp_libclient import rpc, subscriptions
+from openerp_libclient import rpc, agent_commands
 
 rpc.openSession(proto="http", host='localhost', port='8169', 
     user="admin", passwd="admin", dbname="test_bqi")
@@ -41,49 +41,17 @@ class parrot(object):
         return self._parrot_fn(name)
 
 OUR_ADDRESS = 'test-address-1:1'
-OUR_NAME = 'test-commands'
-OUR_INCARNATION = 'tc:%s' % os.getpid()
+# OUR_NAME = 'test-commands'
+# OUR_INCARNATION = 'tc:%s' % os.getpid()
 
 the_parrot = parrot()
-def poll_commands():
-    print "poll commands!"
-    cmds_obj = rpc.RpcProxy('base.command.command')
-    res = cmds_obj.pop_command(address_name=OUR_ADDRESS,
-            agent_name=OUR_NAME, agent_incarnation=OUR_INCARNATION)
-    # print "Command:", res
-    
-    if not res:
-        return
-    
-    try:
-        result = getattr(the_parrot, res['method'])(*(res['args']), **(res['kwargs']))
-    except Exception, e:
-        # FIXME! message, error formulating
-        print "Exception:", e
-        cmds_obj.push_error(res['id'], {'code': 100, 'message': str(e), 'error': e.args[0]})
-    else:
-        cmds_obj.push_result(res['id'], result)
-        
 
-addr_obj = rpc.RpcProxy('base.command.address')
-
-try:
-    addr_id = addr_obj.search([('name','=', OUR_ADDRESS)])
-    if not addr_id:
-        raise ValueError("Address not found")
-    addr_id = addr_id[0]
-    addr_obj.subscribe(addr_id, OUR_NAME, OUR_INCARNATION)
-except Exception, e:
-    print "Cannot subscribe:", e
-    sys.exit(1)
-
-st = subscriptions.SubscriptionThread(OUR_ADDRESS)
-st.setCallback(poll_commands)
-# st.start()
+cmd_thread = agent_commands.CommandsThread(the_parrot, OUR_ADDRESS)
+cmd_thread.start()
 
 try:
     while True:
-        poll_commands()
-        time.sleep(30)
+        print "."
+        time.sleep(60)
 finally:
-    addr_obj.unsubscribe(addr_id, OUR_NAME)
+    cmd_thread.stop()
